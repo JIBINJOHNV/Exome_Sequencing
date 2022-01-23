e577c41cb62c9fb1a07f0732013da924b8a500c9
JIBINJOHNV <69025183+JIBINJOHNV@users.noreply.github.com>
2022-01-23 20:49:31 +0530

    Add files via upload

--- GATK_Annovar/PhenotypeSimilarity/HPO_PHenotype_Annovar.py
+++ GATK_Annovar/PhenotypeSimilarity/HPO_PHenotype_Annovar.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[27]:
+
 
 import pandas as pd
 from difflib import SequenceMatcher
 from fuzzywuzzy import fuzz
 import numpy as np
 import argparse
+
+
+# In[ ]:
 
 
 
@@ -19,22 +24,37 @@ AnnovarFile=args.AnnovarFile
 Phenotype=args.Phenotype
 
 
+# In[ ]:
+
+
 df=pd.read_csv(AnnovarFile,sep="\t")
 phentype=pd.read_csv(Phenotype)
+
+
+# In[28]:
 
 
 #df=pd.read_csv("KT41093424W.hg38_multiannoDesiredColumns.tsv",sep="\t")
 #phentype=pd.read_csv("phenotype.txt")
 
 
+# In[29]:
+
 
 dfHpo=df[['Gene.refGene','HPO_name.refGene']]
 dfHpo['HPO_name.refGene']=dfHpo['HPO_name.refGene'].fillna("No HPO")
 
 
+# In[30]:
+
+
 def similar(a, b):
     tem=fuzz.partial_ratio(a.lower(), b.lower()) 
     return tem
+
+
+# In[22]:
+
 
 
 for j in range(df.shape[0]):
@@ -50,5 +70,17 @@ for j in range(df.shape[0]):
     df.loc[j,"PhenotpeScoreDict"]=str(Dict)
     df.loc[j,"PhenotpeScor"]=Score
 
+         
+
+
+# In[23]:
+
 
 df.to_csv(AnnovarFile[:-4]+"_Phenotype.tsv",sep="\t")
+
+
+# In[ ]:
+
+
+
+
