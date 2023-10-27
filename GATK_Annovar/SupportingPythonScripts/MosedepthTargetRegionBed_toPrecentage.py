8c8b046267d2e0cccf6a9c2bc5041ee5c10e64d6
JIBINJOHNV <69025183+JIBINJOHNV@users.noreply.github.com>
2023-10-27 13:42:51 -0400

    Update MosedepthTargetRegionBed_toPrecentage.py

--- GATK_Annovar/SupportingPythonScripts/MosedepthTargetRegionBed_toPrecentage.py
+++ GATK_Annovar/SupportingPythonScripts/MosedepthTargetRegionBed_toPrecentage.py
@@ -50,9 +50,9 @@ for file in files:
 
 average_depth=pd.DataFrame(coverage_dict).T
 average_depth.columns=list(range(1, 101))
+average_depth.columns=[str(x)+"_X" for x in average_depth ]
 average_depth=average_depth.reset_index()
 average_depth=average_depth.rename(columns={"index":"sample"})
-average_depth.columns=[str(x)+"_X" for x in average_depth ]
 
 
 average_depth.to_csv(Folder+"Average_Exome_coverage_at_differentThreshold.csv",index=None)
